dd6e2dd77c519d90df00fc465b61c9465b50b351
MauriceBruijn <bruijn80@gmail.com>
2017-05-17 10:56:59 +0200

    Application

---
+++ cSharp_WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cSharp_WebApp.Startup))]
+namespace cSharp_WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
