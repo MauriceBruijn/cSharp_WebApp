280194cdcb04dee37877457df8c004f8152e745d
MauriceBruijn <bruijn80@gmail.com>
2017-06-25 19:11:16 +0200

    Model

---
+++ cSharp_WebApp/Models/Songs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cSharp_WebApp.Models
+{
+    public class Songs
+    {
+        public void AddSong()
+        {
+
+        }
+
+        public void DeleteSong()
+        {
+
+        }
+
+        public void UpdateSong()
+        {
+
+        }
+
+        public void GetSong()
+        {
+
+        }
+    }
+}
