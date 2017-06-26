78a0a4da744cf2b1717567feb312cf1fd2c14aee
MauriceBruijn <bruijn80@gmail.com>
2017-06-26 13:54:18 +0200

    4,9/5 klaar met applicatie :)

--- cSharp_WebApp/Models/Songs.cs
+++ cSharp_WebApp/Models/Songs.cs
@@ -34,19 +34,14 @@ namespace cSharp_WebApp.Models
             dtSongs.Columns.Add(dcRating);
 
             ds.ReadXml(HttpContext.Current.Server.MapPath("~/App_Data/Songs.xml"));
-            //HttpContext.Current.Session["xmlDataSet"] = ds;
         }
 
-        public void AddSong(Songs a)
+        public DataRow AllSongs()
         {
-
+            DataRow[] drArray = ds.Tables[0].Rows;
+            return drArray[0];
         }
-
-        public void DeleteSong(Songs d)
-        {
-
-        }
-
+        
         public DataRow GetSong(string id)
         {
             DataRow[] drArray = ds.Tables[0].Select("id=" + id);
@@ -66,9 +61,29 @@ namespace cSharp_WebApp.Models
             SaveSongs();
         }
 
-        public void GetSongs(Songs g)
+        public void AddSong()
         {
-            
+            DataRow dr = ds.Tables[0].NewRow();
+
+            dr["id"] = HttpContext.Current.Request.Form["id"];
+            dr["title"] = HttpContext.Current.Request.Form["title"];
+            dr["artist"] = HttpContext.Current.Request.Form["artist"];
+            dr["playtime"] = HttpContext.Current.Request.Form["playtime"];
+            dr["release"] = HttpContext.Current.Request.Form["release"];
+            dr["rating"] = HttpContext.Current.Request.Form["rating"];
+
+            ds.Tables[0].Rows.Add(dr);
+
+            SaveSongs();
+        }
+
+        public void DeleteSong(string id)
+        {
+            DataRow dr = GetSong(id);
+
+            dr[0].Delete();
+
+            SaveSongs();
         }
 
         public void SaveSongs()
