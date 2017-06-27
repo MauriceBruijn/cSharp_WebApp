b17080d8b61c6c98bc2f441bfc6f36ce346d9650
IngvarSchoenmaker <ingvar@brownfox.nl>
2017-06-27 18:07:39 +0200

    Json toegevoegd

---
+++ cSharp_WebApp/Models/MovieList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cSharp_WebApp.Models
+{
+    public class MovieList
+    {
+
+        public Movie[] Movies { get; set; }
+    }
+
+    public class Movie
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Playtime { get; set; }
+        public string Release { get; set; }
+    }
+
+    
+}
